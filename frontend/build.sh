c960c53eba9c4032f6ff12b15e63407456668db6
Yewo Mhango <mhangoyewoh@gmail.com>
2022-07-03 22:50:59 +0200

    Modified build script to include specific files

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,4 +1,9 @@
 elm-spa build
 rm -r "../app/static/"
 mkdir "../app/static/"
-cp -r ./public/* ../app/static/
+cp -r ./public/dist/ ../app/static/dist/
+mkdir ../app/static/css/
+cp -r ./public/css/Besley/ ../app/static/css/Besley/
+cp -r ./public/css/style.css ../app/static/css/style.css
+cp -r ./public/favicon.ico ../app/static/favicon.ico
+cp -r ./public/logo.svg ../app/static/logo.svg
