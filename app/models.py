aa144314b332ebe8b78ef71a5bab52c34dd4d0f6
Yewo Mhango <mhangoyewoh@gmail.com>
2022-07-03 13:46:53 +0200

    Updated Blog model

--- app/models.py
+++ app/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.utils.text import slugify
 
+from imagekit.models import ImageSpecField  # < here
+from pilkit.processors import ResizeToFill
+
 # Create your models here.
-class Blog:
+
+
+class Blog(models.Model):
     title = models.CharField(max_length=128)
-    subtitle = models.CharField(max_length=256)
-    content = models.CharField()
-    thumbnail = models.ImageField()
-    published = models.DateTimeField(auto_now=True)
+    summary = models.CharField(max_length=256)
+    content = models.TextField()
+    image = models.ImageField(default='', blank=True, upload_to='images')
+    thumbnail = ImageSpecField(source='image',
+                               processors=[ResizeToFill(480, 270)],
+                               format='JPEG',
+                               options={'quality': 60})
+    date = models.DateTimeField(auto_now=True)
     slug = models.SlugField(blank=True, default="")
+    author = models.CharField(max_length=128)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title.value_to_string()
