424f155c1067f0da1541f9e84fd452628e09226a
Yewo Mhango <mhangoyewoh@gmail.com>
2022-11-02 12:41:20 +0200

    Recompiled frontend with changes

--- frontend/build.sh
+++ frontend/build.sh
@@ -6,6 +6,8 @@ mkdir "../app/static/"
 cp -r ./public/dist/ ../app/static/dist/
 mkdir ../app/static/css/
 cp -r ./public/css/Besley/ ../app/static/css/Besley/
+cp -r "./public/css/IBM Plex Mono/" "../app/static/css/IBM Plex Mono/"
 cp -r ./public/css/style.css ../app/static/css/style.css
+cp -r ./public/css/style.css.map ../app/static/css/style.css.map
 cp -r ./public/favicon.ico ../app/static/favicon.ico
 cp -r ./public/logo.svg ../app/static/logo.svg
